a01e1e27b101335da546836f105121cca5de716c
cdw8431 <cdw8431@gmail.com>
2023-08-05 19:45:51 +0900

    fix(node): 활성화 노드 조회시 비활성화 노드 제외 해주는 로직 제거

--- app/domain/node/service.py
+++ app/domain/node/service.py
@@ -8,9 +8,4 @@ class NodeService:
     async def get_active_nodes(self) -> list[str]:
         all_nodes: set[str] = set(await self._node_repo.get_all_nodes())
         active_nodes: set[str] = {node for node in all_nodes if await self._node_repo.is_active(node)}
-
-        should_delete_nodes = all_nodes - active_nodes
-        for node in should_delete_nodes:
-            await self._node_repo.delete_node(node)
-
         return list(active_nodes)
